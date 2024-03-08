1e0abd8f79ef5b6e4468fd659a5941b5cc29cd1c
herezohaib <154750463+herezohaib@users.noreply.github.com>
2024-03-09 03:55:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,115 @@
+let subject1 = 'physics';
+let subject2 = 'Chemistry';
+let subject3 = 'Math';
+
+let subject = ['physics', ' Chemistry', 'Math', ' bio'];
+subject [3] = 'English';
+
+console.log(subject[3 ]);
+var student1 = "zohaib";
+var student2 = "king";
+
+var student = ['zohaib', 'bijji', 'muzna', ' laraib', ];
+
+student [2] = "mini";
+
+console.log(student);
+
+var student = [];
+student[student.length] = "zohaib"
+student[student.length] = "bijji"     
+console.log(student);   
+
+var student = ["zohaib", "bijji"];
+
+student[5] = "Abc";
+console.log(student);
+
+var student = ["zohaib", "bijji",  "muzna", " laraib", ];
+
+var returnValue = student.push('king','mini', 'maxi');
+ var returnValue = student.pop();
+ console.log(returnValue);
+console.log(student);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
